1ab6bd7d0422a5ed7b319a44ae6ed33ddb26aa03
HemanthTech-freak <62098508+HemanthTech-freak@users.noreply.github.com>
2020-04-13 15:43:47 +0530

    shoppingCartVM & CartController

---
+++ BulkyBook.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
